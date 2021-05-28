3b39540c2a953777c4c4bf2773adbf4182c36442
Kevin Jo <kevinhjo725@gmail.com>
2021-05-28 18:33:15 -0400

    Add new_move_positions

--- W3D5/knights_travails/knight_path_finder.rb
+++ W3D5/knights_travails/knight_path_finder.rb
@@ -7,16 +7,34 @@ class KnightPathFinder
         positions = []
 
         moves = [2, -2, 1, -1].permutation(2).to_a.uniq.select { |sub| sub.uniq == sub && sub[0] + sub[1] != 0 }
+        moves.each do |move|
+            new_position = []
+            
+            move.each_with_index do |num, i|
+                new_position << pos[i] + num
+            end
 
+            positions << new_position if new_position[0] >= 0 && new_position[0] <= 8 && new_position[1] >= 0 && new_position[1] <= 8
+        end
+
+        positions
     end
 
     def initialize(start)
         @start = start
         @root_node = PolyTreeNode.new(start)
         @move_tree = build_move_tree
+        @considered_positions = []
+    end
+    
+    def new_move_positions(pos)
+        new_moves = KnightPathFinder.valid_moves(pos)
+        new_moves.reject! { |move| @considered_positions.include?(move) }
+        @considered_positions += new_moves
+        new_moves
     end
 
     def build_move_tree
-
+        
     end
 end

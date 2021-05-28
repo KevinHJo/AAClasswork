3759651bbc6fee1d291a8097ad82892031c703ec
patrickc <patrick.evan.campbell@gmail.com>
2021-05-28 15:08:23 -0600

    qwe

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class PolyTreeNode
     attr_reader :children, :parent, :value
     
@@ -8,13 +10,34 @@ class PolyTreeNode
     end
 
     def parent=(node)
-        unless node.nil?
-            unless parent.nil?
-                @parent.children.delete
+        if node.nil?
+            @parent = node
+        else
+            unless self.parent.nil?
+                @parent.children.delete(self)
             end
             
             @parent = node
             node.children << self unless node.children.include?(self)
         end
+    end
+
+    def add_child(child_node)
+        @children << child_node unless self.children.include?(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "node is not a child" unless self.children.include?(child_node)
+        child_node.parent = nil
+    end
+
+    def dfs(target)
+        return self if self.value == target
+
+        self.children.each do |child|
+            child.dfs(target)
+        end
+        return nil
     end
 end

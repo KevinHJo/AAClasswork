b48cc5e455b5836cc68c62b6e0653c0b7c7df271
Kevin Jo <kevinhjo725@gmail.com>
2021-06-01 17:49:49 -0400

    Add class_inheritance

--- W4D2/class_inheritance.rb
+++ W4D2/class_inheritance.rb
@@ -1,1 +1,38 @@
 require 'byebug'
+
+class Employee
+    attr_reader :salary
+
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
+    end
+
+    def bonus(multiplier)
+        bonus = @salary * multiplier
+    end
+end
+
+class Manager < Employee
+    attr_reader :employees
+
+    def initialize(name, title, salary, boss)
+        @employees = []
+        super
+    end
+
+    def add_employee(name, title, salary)
+        @employees << Employee.new(name, title, salary, self)
+    end
+
+    def bonus(multiplier)
+        sum = 0
+        @employees.each do |e|
+            sum += e.salary
+        end
+
+        bonus = sum * multiplier
+    end
+end

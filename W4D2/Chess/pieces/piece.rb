bbd87ee0bf3044eb7faefb023f78247caea29dd5
Kevin Jo <kevinhjo725@gmail.com>
2021-06-01 20:22:22 -0400

    Add piece subclasses

--- W4D2/Chess/pieces/piece.rb
+++ W4D2/Chess/pieces/piece.rb
@@ -8,9 +8,22 @@ class Piece
     end
 
     def to_s
-
     end
 
     def empty?
+    end
+
+    def valid_moves
+    end
+
+    def pos=(val)
+    end
+
+    def Symbol
+    end
+
+    private
+
+    def move_into_check?(end_pos)
     end
 end
--- W4D2/Chess/slideable.rb
+++ W4D2/Chess/slideable.rb
@@ -1,16 +1,22 @@
 require "byebug"
 
 module Slideable
-
-HORIZANTAL_DIRS = [[1,0],[-1,0],[0,1],[0,-1]]
-DIAGONAL_DIRS = [[1,1],[-1,1],[-1,-1],[1,-1]]
+    HORIZONTAL_DIRS = [[1,0],[-1,0],[0,1],[0,-1]]
+    DIAGONAL_DIRS = [[1,1],[-1,1],[-1,-1],[1,-1]]
 
     def horizontal_dirs
-        return HORIZANTAL_DIRS
+        return HORIZONTAL_DIRS
     end
 
     def diagonal_dirs
         return DIAGONAL_DIRS
     end
 
+    def moves
+        
+    end
+
+    def move_dirs
+
+    end
 end

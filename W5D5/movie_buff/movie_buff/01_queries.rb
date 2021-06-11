cc016e10ad7d0a679899fd60b1c475cc1b589fe3
Kevin Jo <kevinhjo725@gmail.com>
2021-06-11 17:56:51 -0400

    Finish 01_queries.rb

--- W5D5/movie_buff/movie_buff/01_queries.rb
+++ W5D5/movie_buff/movie_buff/01_queries.rb
@@ -43,7 +43,12 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
-
+  Movie
+    .select(:id, :title)
+    .joins(:castings)
+    .group(:id)
+    .order('COUNT(*) DESC')
+    .limit(3)
 end
 
 def directed_by_one_of(them)
@@ -59,6 +64,10 @@ def directed_by_one_of(them)
   #
   # Find the id and title of all the movies directed by one of 'them'.
 
+  Movie
+    .select(:id, :title)
+    .joins(:director)
+    .where(actors: {name: them} )
 end
 
 def movie_names_before_1940
@@ -72,5 +81,7 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie
+    .where('yr < 1940')
+    .pluck(:title)
 end

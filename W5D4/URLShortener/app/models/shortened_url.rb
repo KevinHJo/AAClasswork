83080041054d63c85605bdf99ad97b10975d36c3
Jenny Nhan <j3nnynhan@gmail.com>
2021-06-10 17:05:49 -0700

    worked on shortenedurl model

---
+++ W5D4/URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,21 @@
+require "securerandom"
+
+class ShortenedUrl < ApplicationRecord
+    validates :short_url, uniqueness: true
+    validates :user_id, presence: true
+
+    def self.random_code
+        rand = "shorturl.at/#{SecureRandom.urlsafe_base64}"
+
+        until !ShortenedUrl.exists?(short_url:rand)
+            rand = "shorturl.at/#{SecureRandom.urlsafe_base64}"
+        end
+
+        rand
+    end
+
+    def self.create!(user, extended_url)
+        ShortenedUrl.create(user_id:user.id, long_url:extended_url, short_url: ShortenedUrl.random_code)
+    end
+
+end

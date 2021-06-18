c7681a59196500f3f77fdaf5956d66a1fdbcdd30
Kevin Jo <kevinhjo725@gmail.com>
2021-06-18 17:21:16 -0400

    Create cat model

---
+++ W6D5/ninety_nine_cats/app/models/cat.rb
@@ -0,0 +1,31 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'action_view'
+
+class Cat < ApplicationRecord
+    COLORS = ['blank', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'black', 'white', 'gray', 'clear', 'tie-dye', 'glitter', 'pink', 'multicolor', 'gold', 'silver', 'black and white']
+
+    include ActionView::Helpers::DateHelper
+
+    validates :birth_date, presence: true
+    validates :color, presence: true, inclusion: { in: COLORS }
+    validates :name, presence: true
+    validates :sex, presence: true, inclusion: { in: %w(m f t) }
+    validates :description, presence: true
+
+    def age
+        time_ago_in_words(self.birth_date)
+    end
+end

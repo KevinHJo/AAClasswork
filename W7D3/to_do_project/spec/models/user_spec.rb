4a23881e1f876fb92f7779d62c08d7823cba8add
Kevin Jo <kevinhjo725@gmail.com>
2021-06-23 16:31:03 -0400

    Add specs for user model

--- W7D3/to_do_project/spec/models/user_spec.rb
+++ W7D3/to_do_project/spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:session_token) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length(:password).is_at_least(6) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_uniqueness_of(:session_token) }
 end
